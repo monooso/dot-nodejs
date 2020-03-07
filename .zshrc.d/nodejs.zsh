a4007905ef8f37ae05228e2bfc1b045c1a39115e
Stephen Lewis <code@stephenlewis.me>
2020-03-07 19:39:13 +0000

    Initial commit of migrated dotfiles

---
+++ .zshrc.d/nodejs.zsh
@@ -0,0 +1,5 @@
+# ------------------------------------------------
+# Node.js
+# ------------------------------------------------
+export PATH=./node_modules/.bin:$PATH
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
---
+++ README.md
@@ -0,0 +1,2 @@
+# Node.js dotfiles
+Node.js dotfiles, [managed by GNU Stow](https://brandon.invergo.net/news/2012-05-26-using-gnu-stow-to-manage-your-dotfiles.html).
