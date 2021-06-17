b11bcad1a71b3d481bb3f3e80e6214d4b9a1b579
Stephen Lewis <code@stephenlewis.me>
2021-06-17 09:01:47 +0100

    Install nvm as an antigen bundle

--- .zshrc.d/nodejs.zsh
+++ .zshrc.d/nodejs.zsh
@@ -3,3 +3,10 @@
 # ------------------------------------------------
 export PATH=./node_modules/.bin:$PATH
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
+# ------------------------------------------------
+# Install / load NVM
+# ------------------------------------------------
+export NVM_AUTO_USE=true
+export NVM_COMPLETION=true
+antigen bundle lukechilds/zsh-nvm
